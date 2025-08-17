ef8c9f6186a8e9bc2d94d60b3f0a2abff47454a1
krishnasingh16 <krishnasingh12sep@gmail.com>
2025-08-17 14:48:27 +0100

    jj

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser())
 const corsOption ={
-      origin:"https://back-manager.onrender.com",
+      origin:"*",
       credentials:true
 }
 app.use(cors(corsOption))
